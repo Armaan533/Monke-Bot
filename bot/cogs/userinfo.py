9b28e25caf0e7658f4eedac43fe115145af0bc8a
Armaan <mohammadarmaan533@gmail.com>
2022-06-10 17:31:48 +0530

    writing some code

--- bot/cogs/userinfo.py
+++ bot/cogs/userinfo.py
@@ -12,5 +12,34 @@ class Userinfo(commands.Cog):
         self.client = client
 
     @commands.command(aliases = ["Userinfo","Memberinfo"])
-    async def userinfo(self, ctx: commands.Context, member):
-        print(member)
+    async def userinfo(self, ctx: commands.Context, member: str):
+        if member.startswith("<@") and member.endswith(">"):
+            id = int(member.lstrip("<@").rstrip(">"))
+
+        elif member.startswith("<@!") and member.endswith(">"):
+            id = int(member.lstrip("<@!").rstrip(">"))
+        else:
+            id = member
+
+        user = discord.utils.get(ctx.guild.members, id = id)
+        
+        if user == None:
+            pass
+        else:
+            userinfoEmbed = discord.Embed(
+                title = user.name + "#" + user.discriminator,
+                color = lgd.hexConvertor(mn.colorCollection.find({},{"_id": 0, "Hex": 1}))
+            )
+
+            userinfoEmbed.add_field(
+                name = "Joined at",
+                value = user.joined_at.utcnow(),
+                inline = True
+            )
+
+            userinfoEmbed.add_field(
+                name = "Created at",
+                value = user.created_at.utcnow(),
+                inline = True
+            )
+        
