aaf4dcaef853e40b424f3af2719e4a88c883ecc3
Armaan <mohammadarmaan533@gmail.com>
2022-06-17 16:56:51 +0530

    added some listeners and modified old ones

--- bot/cogs/logs.py
+++ bot/cogs/logs.py
@@ -2,6 +2,7 @@ import discord, asyncio, datetime
 import logical_definitions as lgd
 import mongo_declaration as mn
 from discord.ext import commands
+from datetime import datetime
 
 class Logs(commands.Cog):
 	def __init__(self, client):
@@ -32,34 +33,59 @@ class Logs(commands.Cog):
 		if mn.guildpref.find_one({"_id": str(member.guild.id)},{"_id":0,"Logs":1})["Logs"] == False:
 			pass
 		else:
-			time = member.created_at.utcnow()
+			createtime = member.created_at()
+			createtimestamp = datetime.timestamp(createtime)
 			logChannel = discord.utils.get(member.guild.text_channels, id = int(mn.guildpref.find_one({"_id": str(member.guild.id)},{"_id":0,"Logs":1})["Logs"]))
 			memberjoinLogEmbed = discord.Embed(
 				title = "**Member Joined**",
-				description = f"{member.mention} joined\n**Account Creation**\n{time}",
-				timestamp=datetime.datetime.utcnow()
+				description = f"{member.mention} joined\n**Account Creation**\n<t:{createtimestamp}:F>",
+				timestamp = datetime.utcnow()
 			)
-			memberjoinLogEmbed.set_footer(text = '\u200b')
+			memberjoinLogEmbed.set_footer(text = "Logs by Monke Bot", icon_url = self.client.user.avatar_url)
 			await logChannel.send(embed = memberjoinLogEmbed)
 
 	@commands.Cog.listener()
-	async def on_raw_member_leave(self, payload):
+	async def on_member_remove(self, member: discord.Member):
+		if mn.guildpref.find_one({"_id":str(member.guild_id)},{"_id":0,"Logs":1})["Logs"] == False:
+			pass
+		else:
+			Memberguild = discord.utils.get(self.client.guilds, id = member.guild.id)
+			logChannel = discord.utils.get(Memberguild.text_channels,
+										   id = int(mn.guildpref.find_one({"_id": str(member.guild_id)},{"_id":0,"Logs":1})["Logs"]))
+			memberleaveLogEmbed = discord.Embed(
+				title = "**Member Left**",
+				description = f"{member.mention} left",
+				timestamp = datetime.utcnow()
+			)
+			memberleaveLogEmbed.set_footer(text = "Logs by Monke Bot", icon_url = self.client.user.avatar_url)
+			memberleaveLogEmbed.set_thumbnail(url = member.avatar_url)
+			await logChannel.send(embed = memberleaveLogEmbed)
+
+	@commands.Cog.listener()
+	async def on_raw_message_delete(self, payload: discord.RawMessageDeleteEvent):
 		if mn.guildpref.find_one({"_id":str(payload.guild_id)},{"_id":0,"Logs":1})["Logs"] == False:
 			pass
 		else:
 			Memberguild = self.client.get_guild(payload.guild_id)
-			logChannel = discord.utils.get(Memberguild.text_channels,
-										   id = int(mn.guildpref.find_one({"_id": str(payload.guild_id)},{"_id":0,"Logs":1})["Logs"]))
-			memberleaveLogEmbed = discord.Embed(
-				title = "**Member Left**",
-				description = f"{payload.user.mention} left",
-				timestamp = datetime.datetime.utcnow()
-			)
-			memberleaveLogEmbed.set_footer(text = "\u200b")
-			await logChannel.send(embed = memberleaveLogEmbed)
-
-	# @commands.Cog.listener()
-	# async def on
+			logchannel = discord.utils.get(Memberguild.text_channels, id = int(mn.guildpref.find_one({"_id": str(payload.guild_id)},{"_id":0,"Logs":1})["Logs"]))
+			message = payload.cached_message
+			if message != None:
+				author = message.author
+				cachemessagedeleteEmbed = discord.Embed(
+					title = f"Message Deleted By {author.name}#{author.discriminator}",
+					description = f"Message Contents: ``{message.content}``",
+					timestamp = datetime.utcnow(),
+					color = lgd.hexConvertor(mn.colorCollection.find({},{"_id":0,"Hex":1}))
+				)
+				cachemessagedeleteEmbed.set_footer(text = "\u200b", icon_url = self.client.user.avater_url)
+				await logchannel.send(embed = cachemessagedeleteEmbed)
+			else:
+				noCacheMessageDeleteEmbed = discord.Embed(
+					title = f"Message Deleted in <#{payload.channel_id}",
+					description = "Message Content not available",
+					timestamp = datetime.utcnow(),
+					color = lgd.hexConvertor(mn.colorCollection.find({},{"_id":0, "Hex": 1}))
+				)
 
 def setup(client):
 	client.add_cog(Logs(client))
