0213276dc86a822fe5c076089d3811ef1a20d1c5
Armaan <mohammadarmaan533@gmail.com>
2022-06-19 13:37:58 +0530

    debugging on_raw_message_delete

--- bot/cogs/logs.py
+++ bot/cogs/logs.py
@@ -33,7 +33,7 @@ class Logs(commands.Cog):
 		if mn.guildpref.find_one({"_id": str(member.guild.id)},{"_id":0,"Logs":1})["Logs"] == False:
 			pass
 		else:
-			createtime = member.created_at()
+			createtime = member.created_at
 			createtimestamp = datetime.timestamp(createtime)
 			logChannel = discord.utils.get(member.guild.text_channels, id = int(mn.guildpref.find_one({"_id": str(member.guild.id)},{"_id":0,"Logs":1})["Logs"]))
 			memberjoinLogEmbed = discord.Embed(
@@ -46,7 +46,7 @@ class Logs(commands.Cog):
 
 	@commands.Cog.listener()
 	async def on_member_remove(self, member: discord.Member):
-		if mn.guildpref.find_one({"_id":str(member.guild_id)},{"_id":0,"Logs":1})["Logs"] == False:
+		if mn.guildpref.find_one({"_id":str(member.guild.id)},{"_id":0,"Logs":1})["Logs"] == False:
 			pass
 		else:
 			Memberguild = discord.utils.get(self.client.guilds, id = member.guild.id)
@@ -71,9 +71,13 @@ class Logs(commands.Cog):
 			message = payload.cached_message
 
 			async for entry in Memberguild.audit_logs(limit = 1, action = discord.AuditLogAction.message_delete):
-				deleter = entry.user
 				sender = entry.target
-				print(sender)
+				print(type(sender))
+				if sender == message.author:
+					deleter = entry.user
+				else:
+					deleter = message.author
+
 			msgchannel = discord.utils.get(Memberguild.text_channels, id = payload.channel_id)
 			if message != None:
 				cachemessagedeleteEmbed = discord.Embed(
